e9fd6822fe759c94ff7aca21f9906d6732517ea3
1479454109 <1479454109@qq.com>
2024-03-06 15:03:05 +0800

    提交项目

---
+++ src/pages/index/index.vue
@@ -0,0 +1,26 @@
+
+
+<template>
+  <Navbar />
+  <XtxSwiper />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import Navbar from './componet/navbar.vue'
+import { onLoad } from '@dcloudio/uni-app'
+import { getHomeBannerAPI } from '@/services/home'
+
+//const Navbar = defineAsyncComponent(() => import('./componet/navbar.vue'))
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  console.log(res)
+}
+onLoad(() => {
+  getHomeBannerData()
+})
+</script>
+
+<style lang="scss">
+//
+</style>
---
+++ src/utils/aaa.js
@@ -0,0 +1,4 @@
+let aaa={
+    name:'张三',
+    age:18,
+}
